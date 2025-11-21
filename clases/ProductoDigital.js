68f9f28c01becef1478610475c6126482f1e7671
JVillarMel <43390164+meleiro@users.noreply.github.com>
2025-11-21 17:00:24 +0100

    Update ProductoDigital.js

--- clases/ProductoDigital.js
+++ clases/ProductoDigital.js
@@ -2,5 +2,17 @@ const Producto = require("./Producto");
 
 class ProductoDigital extends Producto {
 
-    
+    constructor(nombre, precio, stock, tamDescarga) {
+        super(nombre, precio, stock);
+        this.tamDescarga = tamDescarga;
+    }
+
+
+    mostrarInfo() {
+        super.mostrarInfo();
+        console.log(`Tamaño descarga: ${this.tamDescarga} MB`);
+    }
+
 }
+
+module.exports = ProductoDigital;
