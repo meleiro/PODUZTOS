e9315ee62de9b727d4a9daa6a3361ec1f8d68e35
pepe <macendo@gmail.com>
2026-01-12 12:43:35 +0100

    PRODUZTOS 09012026

--- model/ProductosRepo.js
+++ model/ProductosRepo.js
@@ -2,20 +2,3 @@
    SELECCIÓN DINÁMICA DE CAPA DE DATOS
    ============================ */
 
-/*
-   Se obtiene el modo de acceso a datos desde una variable de entorno.
-   - DATA_LAYER puede valer, por ejemplo: "DAO" o "ORM"
-   - Si no está definida, se usa "DAO" por defecto
-   - Se pasa a mayúsculas para evitar errores por diferencias de formato
-*/
-const modo = (process.env.DATA_LAYER || "DAO").toUpperCase();
-
-/*
-   Se exporta dinámicamente el módulo correspondiente según el modo elegido:
-   - Si el modo es "ORM", se usa la implementación basada en ORM (Prisma, Sequelize, etc.)
-   - En cualquier otro caso, se usa la implementación DAO (acceso manual a BD)
-*/
-module.exports =
-  modo === "ORM"
-    ? require("./ProductosORM")
-    : require("./ProductosDAO");
